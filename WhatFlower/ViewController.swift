3d9a060babc28381ff3d0ad4f16b3d979f53fbde
Sergey <Starchenkov>
2021-01-28 17:11:50 +0700

    Add ML model, detect func.

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -31,8 +33,42 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             
             imageView.image = userPickerImage
             
+            guard let ciimage = CIImage(image: userPickerImage) else {
+                fatalError("Error creating ciimage")
+            }
+            
+            detect(imageFlower: ciimage)
+            
         }
         pickerImage.dismiss(animated: true, completion: nil)
+    }
+    
+    // Add detect function with MLModel
+    func detect(imageFlower: CIImage) {
+        
+        // Create model ML
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier(configuration: .init()).model) else {
+            fatalError("Error creating model ML")
+        }
+        
+        // Create request
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let result = request.results as? [VNClassificationObservation] else {
+                fatalError("Error request")
+            }
+            if let firstResult = result.first?.identifier {
+                self.navigationItem.title = firstResult
+            }
+        }
+        
+        // Create handler
+        let handler = VNImageRequestHandler(ciImage: imageFlower)
+        do{
+            try handler.perform([request])
+        }catch{
+            print ("Error request perfome \(error)")
+        }
+        
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
