917ca2ccbe1a27499d70cc01c9bd61e50e467767
Sergey <Starchenkov>
2021-01-28 14:18:12 +0700

    Added UI Elements.

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        
+        
+    }
 }
 
